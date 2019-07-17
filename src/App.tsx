7acf111a0d5bbb91dd73a3131ba34e697353ae00
Vincent <git@vincenttunru.com>
2019-07-17 14:34:09 +0200

    Adding basic authentication functionality
    To be able to write to a person's Pod, they need to log in and
    authorise our app. To do this, we use the React components in
    https://www.npmjs.com/package/@solid/react.
    
    Although it unfortunately comes with a bunch of stuff we will not
    be using, the authentication components are easy enough to use:
    wrap components that should be visible to people who are not
    logged in inside a <LoggedOut> component, and those that should
    only be visible to those who are logged in inside a <LoggedIn>.
    
    Actual authentication is initiated with a <LoginButton>.
    Unfortunately, at this time this involves including an HTML blob
    verbatim as downloaded from
    https://solid.github.io/solid-auth-client/dist/popup.html
    (this is linked to from the README of @solid/react). In the future,
    this will hopefully be properly packaged.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { LoggedOut, LoginButton, LoggedIn } from '@solid/react';
+import { Dashboard } from './components/Dashboard';
 
 const App: React.FC = () => {
-  return (
-    <p>This is a basic app that can use <a href="https://bulma.io/" title="The Bulma CSS framework">Bulma</a> for styling.</p>
-  );
+  return <>
+    <LoggedOut>
+      <section className="section">
+        <p className="content">Please login in to your Pod to start keeping track of the movies you have seen.</p>
+        <p className="content">
+          <LoginButton popup="popup.html" className="button is-large is-primary">Log in</LoginButton>
+        </p>
+      </section>
+    </LoggedOut>
+    <LoggedIn>
+      <Dashboard/>
+    </LoggedIn>
+  </>;
 }
 
 export default App;
