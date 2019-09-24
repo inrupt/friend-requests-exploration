6f2b1cf4d3ac4e17170985d3d28a8b3d8eba33f2
Michiel de Jong <michiel@unhosted.org>
2019-09-24 11:31:05 +0200

    ProviderLogin

--- src/pages/Consumer.tsx
+++ src/pages/Consumer.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import { RouteComponentProps } from '@reach/router';
+import { withWebId, ProviderLogin } from '@inrupt/solid-react-components';
 
 type Props = RouteComponentProps;
 
 export const Consumer: React.FC<Props> = (props) => {
-  return <>
-    Consumer
-  </>;
+  const MyComponent = withWebId((webIdProps:any) => {
+    if (webIdProps.webID) {
+      return <> hello {webIdProps.webID} </>;
+    } else {
+      return <ProviderLogin />;
+    }
+  });
+  return <MyComponent />;
 };
