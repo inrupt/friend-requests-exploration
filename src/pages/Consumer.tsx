ad4f60571fca3d5a37b814f30eacbbc89e6ac89b
Michiel de Jong <michiel@unhosted.org>
2019-09-25 12:21:41 +0200

    Switch from 'full access' design to 'paste' design

--- src/pages/Consumer.tsx
+++ src/pages/Consumer.tsx
@@ -1,44 +1,9 @@
 import React from 'react';
 import { RouteComponentProps } from '@reach/router';
-import { withWebId, ProviderLogin, LogoutButton } from '@inrupt/solid-react-components';
-import { fetchDocument } from 'tripledoc';
+import { CorroboratingEventForm } from '../consumer/components/CorroboratingEventForm';
 
 type Props = RouteComponentProps;
 
-async function getInboxItems(webId: string): Promise<Array<string>> {
-  /* 1. Fetch the Document at `webId`: */
-  const webIdDoc = await fetchDocument(webId);
-  /* 2. Read the Subject representing the current user: */
-  const profile = webIdDoc.getSubject(webId);
-  /* 3. Get their ldp:inbox: */
-  const inboxUri = profile.getNodeRef('http://www.w3.org/ns/ldp#inbox');
-  if (!inboxUri) {
-    return [];
-  }
-  const inboxDoc = await fetchDocument(inboxUri);
-  const inboxThing = inboxDoc.getSubject('')
-  const items = inboxThing.getAllNodeRefs('http://www.w3.org/ns/ldp#contains')
-  return items
-}
-
-export const Consumer: React.FC<Props> = (props) => {
-  const MyComponent = withWebId((webIdProps:any) => {
-    console.log(webIdProps)
-    if (webIdProps.webId) {
-      getInboxItems(webIdProps.webId).then((inboxItems: Array<string>) => {
-        console.log(inboxItems)
-      }).catch((err: Error) => {
-        console.error(err.message);
-      })
-      return <>
-        hello {webIdProps.webId}, see the console for your inbox items!
-        < LogoutButton />
-      </>;
-    } else {
-      return <ProviderLogin
-        callbackUri={window.location.href}
-      />;
-    }
-  });
-  return <MyComponent />;
+export const Consumer: React.FC<Props> = (props: any) => {
+  return <CorroboratingEventForm/>;
 };
