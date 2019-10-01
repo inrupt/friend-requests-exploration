77a2490888aa641c96b665c191f8d1503045094c
Vincent <git@vincenttunru.com>
2019-10-01 11:42:35 +0200

    Add note about Control access

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ const App: React.FC = () => {
   return <>
     <LoggedOut>
       <section className="section">
+        <p className="content">This app requires you to log in. It requires <b>Control</b> access.</p>
         <LoginButton popup="popup.html" className="button is-large is-primary">Log in to start using</LoginButton>
       </section>
     </LoggedOut>
