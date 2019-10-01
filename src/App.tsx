54b8c5be17e52948f888fdafd7d9ff4d8d0b28d2
Vincent <git@vincenttunru.com>
2019-10-01 11:36:16 +0200

    Enforce login

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import { FriendList } from './components/FriendList';
+import { LoggedOut, LoginButton, LoggedIn } from '@solid/react';
 
 const App: React.FC = () => {
   return <>
-    <FriendList/>
+    <LoggedOut>
+      <section className="section">
+        <LoginButton popup="popup.html" className="button is-large is-primary">Log in to start using</LoginButton>
+      </section>
+    </LoggedOut>
+    <LoggedIn>
+      <FriendList/>
+    </LoggedIn>
   </>;
 }
 
