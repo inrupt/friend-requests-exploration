0766863d3211070d62df23587391fff5f4188d3d
Vincent <git@vincenttunru.com>
2019-11-13 12:12:16 +0100

    Enable React's Strict Mode

--- src/App.tsx
+++ src/App.tsx
@@ -11,36 +11,38 @@ import { Profile } from './components/Profile';
 
 const App: React.FC = () => {
   return <>
-    <Router>
-      <LoggedOut>
-        <section className="section">
-          <h1 className="title">Friend Requests Exploration</h1>
-          <p className="subtitle">This app requires you to log in.
-          Unless launched from the <a href="https://launcher-exploration.inrupt.app/">Launcher app</a>,
-          it requires pod-wide <strong>Control</strong> access.</p>
-          <LoginButton popup="popup.html" className="button is-large is-primary">Log in to start using this app</LoginButton>
-        </section>
-      </LoggedOut>
-      <LoggedIn>
-        <div className="panel">
-          <p className="subtitle">Logged in as {useWebId()}.</p>
-          <LogoutButton className="button is-warning" />
-        </div>
-        <Switch>
-          <Route path="/profile/:webId">
-            <Profile/>
-          </Route>
-          <Route path="/">
-            <div className="panel">
-              <IncomingList/>
-            </div>
-            <div className="panel">
-              <FriendLists/>
-            </div>
-          </Route>
-        </Switch>
-      </LoggedIn>
-    </Router>
+    <React.StrictMode>
+      <Router>
+        <LoggedOut>
+          <section className="section">
+            <h1 className="title">Friend Requests Exploration</h1>
+            <p className="subtitle">This app requires you to log in.
+            Unless launched from the <a href="https://launcher-exploration.inrupt.app/">Launcher app</a>,
+            it requires pod-wide <strong>Control</strong> access.</p>
+            <LoginButton popup="popup.html" className="button is-large is-primary">Log in to start using this app</LoginButton>
+          </section>
+        </LoggedOut>
+        <LoggedIn>
+          <div className="panel">
+            <p className="subtitle">Logged in as {useWebId()}.</p>
+            <LogoutButton className="button is-warning" />
+          </div>
+          <Switch>
+            <Route path="/profile/:webId">
+              <Profile/>
+            </Route>
+            <Route path="/">
+              <div className="panel">
+                <IncomingList/>
+              </div>
+              <div className="panel">
+                <FriendLists/>
+              </div>
+            </Route>
+          </Switch>
+        </LoggedIn>
+      </Router>
+    </React.StrictMode>
   </>;
 }
 
