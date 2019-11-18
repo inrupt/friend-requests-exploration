02378d5135986d2c796d92ab45ba5c0ec9c894b4
Michiel de Jong <michiel@unhosted.org>
2019-11-18 13:58:25 +0100

    Catch unreadable inbox docs, ref #50

--- src/services/getFollowAction.ts
+++ src/services/getFollowAction.ts
@@ -8,14 +8,19 @@ const as = {
 };
 
 export async function getFollowAction(inboxRef: Reference): Promise<TripleSubject | null> {
-  const inboxDocument = await getDocument(inboxRef);
-  const befriendActions = inboxDocument.getSubjectsOfType(as.Follow);
+  try {
+    const inboxDocument = await getDocument(inboxRef);
+    const befriendActions = inboxDocument.getSubjectsOfType(as.Follow);
 
-  // If an inbox item is not exactly in the format we expect,
-  // it's probably not what we're looking for:
-  if (befriendActions.length !== 1) {
+    // If an inbox item is not exactly in the format we expect,
+    // it's probably not what we're looking for:
+    if (befriendActions.length !== 1) {
+      return null;
+    }
+
+    return befriendActions[0];
+  } catch (e) {
+    console.error('unreadable inbox document', inboxRef);
     return null;
   }
-
-  return befriendActions[0];
 }
