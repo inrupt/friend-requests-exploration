2229bf4c3591754423d86d0ff463758febe5bf3b
Michiel de Jong <michiel@unhosted.org>
2019-11-25 12:43:38 +0100

    use Route only on MainPanel

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import { IncomingList } from './components/IncomingList';
 import { LoggedOut, LoginButton, LoggedIn, LogoutButton, useWebId } from '@solid/react';
 import { MainPanel, PersonSummary } from './components/Person';
@@ -8,7 +8,7 @@ import { FriendList } from './components/Friendlist';
 const App: React.FC = () => {
   return <>
     <React.StrictMode>
-      <Router>
+      <BrowserRouter>
         <LoggedOut>
           <section className="section">
             <h1 className="title">Friend Requests Exploration</h1>
@@ -19,44 +19,43 @@ const App: React.FC = () => {
           </section>
         </LoggedOut>
         <LoggedIn>
+          <div className="container">
+            <nav className="navbar has-shadow">
+              <div className="navbar-start">
+                <PersonSummary webId={(useWebId() || undefined)} />
+              </div>
+              <div className="navbar-end">
+                <LogoutButton className="button is-primary"/>
+              </div>
+              <div className="nav-toggle">
+                <span></span>
+                <span></span>
+                <span></span>
+              </div>
+              <input type="checkbox" id="menu-toggle" className="is-hidden"/>
+              <div className="nav-right nav-menu">
+                <div className="nav-item is-tab is-hidden-tablet">
+                  <span className="icon"><i className="fa fa-home"></i></span> You
+                </div>
+                <div className="nav-item is-tab is-hidden-tablet">
+                  <span className="icon"><i className="fa fa-table"></i></span> Pending
+                </div>
+                <div className="nav-item is-tab is-hidden-tablet">
+                  <span className="icon"><i className="fa fa-info"></i></span> Friends
+                </div>
+                <div className="nav-item is-tab is-hidden-tablet">
+                  <span className="icon"><i className="fa fa-info"></i></span> Blocked
+                </div>
+                <div className="nav-item is-tab is-active">
+                  <span className="icon"><i className="fa fa-user"></i></span>
+                </div>
+                <div className="nav-item is-tab">
+                  <span className="icon"><i className="fa fa-sign-out"></i></span>
+                </div>
+              </div>
+            </nav>
+          </div>
           <Route path="/:webId">
-            <div className="container">
-              <nav className="navbar has-shadow">
-                <div className="navbar-start">
-                  <PersonSummary webId={(useWebId() || undefined)} />
-                </div>
-                <div className="navbar-end">
-                  <LogoutButton className="button is-primary"/>
-                </div>
-                <div className="nav-toggle">
-                  <span></span>
-                  <span></span>
-                  <span></span>
-                </div>
-                <input type="checkbox" id="menu-toggle" className="is-hidden"/>
-                <div className="nav-right nav-menu">
-                  <div className="nav-item is-tab is-hidden-tablet">
-                    <span className="icon"><i className="fa fa-home"></i></span> You
-                  </div>
-                  <div className="nav-item is-tab is-hidden-tablet">
-                    <span className="icon"><i className="fa fa-table"></i></span> Pending
-                  </div>
-                  <div className="nav-item is-tab is-hidden-tablet">
-                    <span className="icon"><i className="fa fa-info"></i></span> Friends
-                  </div>
-                  <div className="nav-item is-tab is-hidden-tablet">
-                    <span className="icon"><i className="fa fa-info"></i></span> Blocked
-                  </div>
-                  
-                  <div className="nav-item is-tab is-active">
-                    <span className="icon"><i className="fa fa-user"></i></span>
-                  </div>
-                  <div className="nav-item is-tab">
-                    <span className="icon"><i className="fa fa-sign-out"></i></span>
-                  </div>
-                </div>
-              </nav>
-            </div>
             <section className="main-content columns is-fullheight">
               <aside className="column is-4 is-narrow-mobile is-fullheight section is-hidden-mobile">
     
@@ -78,6 +77,7 @@ const App: React.FC = () => {
                 </div>
               </div>
             </section>
+            </Route>
             <footer className="footer is-hidden">
               <div className="container">
                 <div className="content has-text-centered">
@@ -85,9 +85,8 @@ const App: React.FC = () => {
                 </div>
               </div>
             </footer>
-          </Route>
         </LoggedIn>
-      </Router>
+      </BrowserRouter>
     </React.StrictMode>
   </>;
 }
