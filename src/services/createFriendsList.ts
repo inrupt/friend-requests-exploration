e10276eddfcbc102498f789b8863f0a3dbd025ba
Michiel de Jong <michiel@unhosted.org>
2019-11-25 16:57:05 +0100

    await createInbox

--- src/services/createFriendsList.ts
+++ src/services/createFriendsList.ts
@@ -31,17 +31,22 @@ async function postDoc(body: string, containerUrl: string) {
 
 async function createAclDoc(webId: string, friendsListUri: string) {
   const friendsListDoc = await getDocument(friendsListUri);
+  console.log('got friendsListDoc', webId, friendsListUri);
   const friendsAclRef = friendsListDoc.getAclRef();
+  console.log('got acl ref', webId, friendsListUri, friendsAclRef);
   if (!friendsAclRef) {
     throw new Error('Could not determine ACL ref for friends list');
   }
   let friendsAclDoc
   try {
     await getDocument(friendsAclRef);
+    console.log('got acl doc');
   } catch (e) {
+    console.log('creating acl doc');
     friendsAclDoc = createDocument(friendsAclRef);
   }
   if (friendsAclDoc) {
+    console.log('have friendsAclDoc');
     const ownerAcl = friendsAclDoc.addSubject();
     ownerAcl.addNodeRef(rdf.type, acl.Authorization);
     ownerAcl.addNodeRef(acl.accessTo, friendsListDoc.asNodeRef());
@@ -57,6 +62,8 @@ async function createAclDoc(webId: string, friendsListUri: string) {
     friendsAcl.addNodeRef(acl.mode, acl.Read);
     friendsAcl.addNodeRef(acl.agentGroup, friendsListUri);
     friendsAclDoc.save();
+  } else {
+    console.log('have no friendsAclDoc');
   }
 }
 
@@ -67,7 +74,8 @@ async function createInbox(podRoot: string) {
   return inboxUrl;
 }
 
-async function linkFromProfile(webId: string, ret: string) {
+async function linkFromProfile(webId: string, friendsListUri: string, inboxUrl: string) {
+  console.log('linkFromProfile', webId, friendsListUri);
   const profileDoc: TripleDocument | null = await getDocument(webId);
   if (!profileDoc) {
     throw new Error('profile doc not fetched!');
@@ -77,26 +85,41 @@ async function linkFromProfile(webId: string, ret: string) {
   if (!profileSub) {
     throw new Error('profile sub not found!');
   }
-  profileSub.addRef(as.following, ret);
-  console.log('friends list created, linking', webId, ret);
+  profileSub.addRef(as.following, friendsListUri);
+  // const remoteSub = profileDoc.addSubject(friendsListUri);
+  // remoteSub.addRef('http://www.w3.org/ns/ldp#inbox', 'inboxUrl');
+  console.log('friends list created, linking', webId, friendsListUri);
   await profileDoc.save();
   
 }
 
+// avoid running this multiple times in parallel:
+const promises: { [webId: string]: Promise<string>} = {};
 export async function createFriendsList(webId: string) {
+  if (!promises[webId]) {
+    promises[webId] = doCreateFriendsList(webId);
+  }
+  return promises[webId];
+}
+
+async function doCreateFriendsList(webId: string) {
   let ret: string | null = null;
   console.log('creating!');
   const podRoot = await getPodRoot(webId);
   if (!podRoot) {
     throw new Error('no podRoot!');
   }
-  const inboxUrl = createInbox(podRoot);
+  const inboxUrl: string = await createInbox(podRoot);
+  console.log('inbox created');
   const body = `<#this> a <http://www.w3.org/2006/vcard/ns#Group> .` +
     `<#this> <http://www.w3.org/2006/vcard/ns#fn> "Solid Friends" .` +
     `<#this> <http://www.w3.org/ns/ldp#inbox> <${inboxUrl}> .`;
   const absoluteLocation = await postDoc(body, podRoot);
   ret = new URL('#this', absoluteLocation).toString();
+  console.log('doc created');
   await createAclDoc(webId, ret);
-  await linkFromProfile(webId, ret);
+  console.log('acl created');
+  await linkFromProfile(webId, ret, inboxUrl);
+  console.log('linked from profile');
   return ret;
 }
