951737ce792919004a0775362cac85cf1ea5da29
Michiel de Jong <michiel@unhosted.org>
2019-11-28 15:37:20 +0100

    queueing with React

--- src/services/usePersonTypeLists.ts
+++ src/services/usePersonTypeLists.ts
@@ -2,6 +2,7 @@ import { PersonType, getPersonDetails, PersonDetails } from "./usePersonDetails"
 import SolidAuth from 'solid-auth-client';
 import { useEffect } from "react";
 import React from "react";
+import { useWebId } from "@solid/react";
 
 export type PersonTypeLists = {
   [type in PersonType]: {
@@ -27,35 +28,42 @@ export function usePersonTypeLists(): PersonTypeLists {
     [PersonType.blocked]: {},
     [PersonType.stranger]: {},
   });
-  // const [queue, setQueue] = React.useState<string[]>([]);
+  const [queue, setQueue] = React.useState<string[]>([]);
+  const me: string | null = useWebId() || null;
 
-  // useEffect(() => {
-  //   async function considerWebId(webId: string) {
-  //     const personDetails = await getPersonDetails(webId);
-  //     if (personDetails && personDetails.personType) {
-  //       if (!lists[personDetails.personType][personDetails.webId]) {
-  //         lists[personDetails.personType][personDetails.webId] = personDetails;
-  //         if (personDetails.follows) {
-  //           setQueue(queue.concat(personDetails.follows));
-  //         }
-  //       }
-  //     }
-  //   }
-  //   async function getAll() {
-  //     // there are two starting points: the current user, and the inbox.
-  //     // from there, keep looking at friends to discover more.
-  //     const me: string | null = await getMyWebId();
-  //     if (me) {
-  //       await considerWebId(me);
-  //       setQueue(queue.concat(await getIncoming()));
-  //       while(queue.length) {
-  //         const batch = [...queue];
-  //         setQueue([]);
-  //         await Promise.all(batch.map(considerWebId));
-  //       }
-  //     }
-  //   }
-  //   getAll();
-  // });
+  let batchNo = 0;
+  useEffect(() => {
+    if (queue.length) {
+      const thisBatchNo = batchNo++;
+      const batch = [...queue];
+      console.log(`Batch ${thisBatchNo}, considering ${queue.length} webId's`, batch);
+      setQueue([]);
+      Promise.all(batch.map(considerWebId)).then(() => {
+        console.log(`Finished batch ${thisBatchNo}`);
+      });
+    }
+    async function considerWebId(webId: string) {
+      const personDetails = await getPersonDetails(webId);
+      if (personDetails && personDetails.personType) {
+        if (!lists[personDetails.personType][personDetails.webId]) {
+          lists[personDetails.personType][personDetails.webId] = personDetails;
+          if (personDetails.follows) {
+            setQueue(queue.concat(personDetails.follows));
+          }
+        }
+      }
+    }
+  }, [queue]);
+  useEffect(() => {
+    console.log('logged in user changed', me);
+    (async function getAll() {
+      // there are two starting points: the current user, and the inbox.
+      // from there, keep looking at friends to discover more.
+      if (!me) {
+        return;
+      }
+      setQueue([me].concat(await getIncoming()));
+    })();
+  }, [me]);
   return lists;
 }
