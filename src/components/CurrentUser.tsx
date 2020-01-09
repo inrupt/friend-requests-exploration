a7fde591b4c4052c009faad36f5bc493f13a5b68
sstratsianis@gmail.com <sstratsianis@gmail.com>
2020-01-09 19:25:26 +1100

    resolved Valid DOM error

--- src/components/CurrentUser.tsx
+++ src/components/CurrentUser.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useWebId } from "@solid/react";
+import { useWebId } from '@solid/react';
 import { PersonSummary } from './PersonLists';
 import { usePersonDetails } from '../services/usePersonDetails';
 
@@ -7,16 +7,18 @@ export const CurrentUser: React.FC<{}> = () => {
   const myWebId = useWebId() || null;
   const myPersonDetails = usePersonDetails(myWebId);
   if (myPersonDetails) {
-    return <div className="navbar-start">
-      You: <PersonSummary details={myPersonDetails} />
-    </div>;
+    return (
+      <div className='navbar-start'>
+        You: <PersonSummary details={myPersonDetails} />
+      </div>
+    );
   }
   if (myWebId) {
-    return <div className="navbar-start">
-      You: <code>{myWebId}</code>>
-    </div>;
+    return (
+      <div className='navbar-start'>
+        You: <code>{myWebId}</code>>
+      </div>
+    );
   }
-  return <div className="navbar-start">
-    (loading your profile...)
-  </div>;
-}
+  return <div className='navbar-start'>(loading your profile...)</div>;
+};
--- src/components/PersonLists.tsx
+++ src/components/PersonLists.tsx
@@ -1,67 +1,77 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { PersonDetails, PersonType } from '../services/usePersonDetails';
-import { usePersonTypeLists, PersonTypeLists } from '../services/usePersonTypeLists';
+import {
+  usePersonTypeLists,
+  PersonTypeLists
+} from '../services/usePersonTypeLists';
 
 interface PersonProps {
-  details: PersonDetails
+  details: PersonDetails;
 }
 
-
-
-export const PersonSummary: React.FC<PersonProps> = (props) => {
+export const PersonSummary: React.FC<PersonProps> = props => {
   const details = props.details;
-  const photo = <>
-    <div className="media-right">
-    <figure className="card-header-title">
-      <p className="image is-48x48">
-        <img src={details.avatarUrl || '/img/default-avatar.png'} alt="Avatar" className="is-rounded"/>
-      </p>
-    </figure>
-    </div>
-  </>;
+  const photo = (
+    <>
+      <div className='media-right'>
+        <figure className='card-header-title'>
+          <p className='image is-48x48'>
+            <img
+              src={details.avatarUrl || '/img/default-avatar.png'}
+              alt='Avatar'
+              className='is-rounded'
+            />
+          </p>
+        </figure>
+      </div>
+    </>
+  );
 
-  return <a href={`/profile/${encodeURIComponent(details.webId)}`}>
-    <Link
-      to={`/profile/${encodeURIComponent(details.webId)}`}
-      title="View this person's friends"
-    >
-      <div className="media">
-        {photo}
-        <div className="media-content">
-          <div className="media-left">
-            {details.fullName}
+  return (
+    <div>
+      <Link
+        to={`/profile/${encodeURIComponent(details.webId)}`}
+        title="View this person's friends"
+      >
+        <div className='media'>
+          {photo}
+          <div className='media-content'>
+            <div className='media-left'>{details.fullName}</div>
           </div>
         </div>
-      </div>
-    </Link>
-  </a>;
-}
+      </Link>
+    </div>
+  );
+};
 
 function getPersonCard(personDetails: PersonDetails): React.ReactElement {
   return (
-    <div key={personDetails.webId} className="card">
-        <PersonSummary details={personDetails}/>
+    <div key={personDetails.webId} className='card'>
+      <PersonSummary details={personDetails} />
     </div>
   );
 }
 
-export const TypeList: React.FC<{ list: { [webId: string]: PersonDetails }, header: string }> = ({ list, header }) => {
+export const TypeList: React.FC<{
+  list: { [webId: string]: PersonDetails };
+  header: string;
+}> = ({ list, header }) => {
   // if (Object.keys(list).length === 0) {
   //   return <></>;
   // }
 
   return (
     <div>
-      <p className="panel-heading">
+      <p className='panel-heading'>
         {header} ({Object.keys(list).length})
       </p>
       <div>
         {Object.keys(list).map((webId: string) => getPersonCard(list[webId]))}
       </div>
     </div>
-    );
-}
+  );
+};
 
 export const DiscoverableLists: React.FC<{}> = () => {
   const lists: PersonTypeLists = usePersonTypeLists();
@@ -69,21 +79,23 @@ export const DiscoverableLists: React.FC<{}> = () => {
     return <>Loading...</>;
   }
 
-  return <div className="menu-list">
-    <nav className="panel">
-      <TypeList header="Inbox" list={lists[PersonType.requester]} />
-    </nav>
-    <nav className="panel">
-      <TypeList header="Sent" list={lists[PersonType.requested]} />
-    </nav>
-    <nav className="panel">
-      <TypeList header="Friends" list={lists[PersonType.friend]} />
-    </nav>
-    <nav className="panel">
-      <TypeList header="Rejected" list={lists[PersonType.blocked]} />
-    </nav>
-    <nav className="panel">
-      <TypeList header="Suggestions" list={lists[PersonType.stranger]} />
-    </nav>
-  </div>;
-}
+  return (
+    <div className='menu-list'>
+      <nav className='panel'>
+        <TypeList header='Inbox' list={lists[PersonType.requester]} />
+      </nav>
+      <nav className='panel'>
+        <TypeList header='Sent' list={lists[PersonType.requested]} />
+      </nav>
+      <nav className='panel'>
+        <TypeList header='Friends' list={lists[PersonType.friend]} />
+      </nav>
+      <nav className='panel'>
+        <TypeList header='Rejected' list={lists[PersonType.blocked]} />
+      </nav>
+      <nav className='panel'>
+        <TypeList header='Suggestions' list={lists[PersonType.stranger]} />
+      </nav>
+    </div>
+  );
+};
